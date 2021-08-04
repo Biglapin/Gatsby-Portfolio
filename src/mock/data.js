bab713d370308007f0a102ace5b2afef484cc82e
kevin SANTANDER <kevin.santander@protonmail.ch>
2021-08-04 19:10:27 +0200

    new folder

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,16 +2,16 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Kevin SANTANDER', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my website, my name is kevin SANTANDER', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Kevin SANTANDER',
+  subtitle: 'JS Developer',
   cta: '',
 };
 
